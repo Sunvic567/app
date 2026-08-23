02f913bab92ad61ddc1a1ade4a46d565b5307a39
sunvic567 <sunvictor567@gmail.com>
2026-08-23 21:54:52 +0100

    Fix missing redirect import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 from  api_routers import documents, followup, sessions
 
