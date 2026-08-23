01ea72ea01810821f8e2b5959c07ccd68d8735b3
sunvic567 <sunvictor567@gmail.com>
2026-08-23 21:41:20 +0100

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-from api_routers import documents, followup, sessions
+from  api_routers import documents, followup, sessions
 
 app = FastAPI(
     title="Accounting Client Onboarding Agent (Demo)",
